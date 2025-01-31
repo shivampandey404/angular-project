525e995e2a9716ba05f1efd37ba70cb50c7fc782
shivampandey404 <shivampandey242524@gmail.com>
2025-01-31 12:23:15 +0530

    Adding Google Authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,10 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: "AIzaSyD3IZybmixYuLDubCFh_aDkfRDJT8zyjBc",
+    authDomain: "friendlychat-568be.firebaseapp.com",
+    projectId: "friendlychat-568be",
+    storageBucket: "friendlychat-568be.firebasestorage.app",
+    messagingSenderId: "714914493444",
+    appId: "1:714914493444:web:5c96c0684261d8add13151",
   },
 };
